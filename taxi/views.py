db73fed418eb5188908e2566eca47f64aafb431e
Viacheslav Nykonenko <v.nykonenko@gmail.com>
2025-07-15 20:53:13 +0300

    Solution. Implemented search features and tests.

--- taxi/views.py
+++ taxi/views.py
@@ -47,7 +47,7 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     paginate_by = 5
 
     def get_context_data(
-        self, *, object_list = None, **kwargs
+        self, *, object_list=None, **kwargs
     ):
         context = super(ManufacturerListView, self).get_context_data(**kwargs)
         name = self.request.GET.get("name", "")
